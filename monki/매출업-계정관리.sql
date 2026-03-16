14819397f2ef0b83be1e1800569bedc720efee15
max <max@monki.net>
2026-03-16 09:11:30 +0900

    매출업-계정관리 수정중

--- monki/매출업-계정관리.sql
+++ monki/매출업-계정관리.sql
@@ -16,3 +16,34 @@ select * from sellup.manager_store where store_no=760;
 INSERT INTO sellup.manager_store (manager_store_id, manager_id, store_no, role,is_active) VALUES (uuid_generate_v4(), 'e73adca8-5b6b-4cee-b932-1cb644f0c6ea', 760, 'MK_ADMIN',true);
 INSERT INTO sellup.manager_store (manager_store_id, manager_id, store_no, role,is_active) VALUES (uuid_generate_v4(), 'e73adca8-5b6b-4cee-b932-1cb644f0c6ea', 687, 'MK_ADMIN',true);
 INSERT INTO sellup.manager_store (manager_store_id, manager_id, store_no, role,is_active) VALUES (uuid_generate_v4(), 'e73adca8-5b6b-4cee-b932-1cb644f0c6ea', 825, 'MK_ADMIN',true);
+
+
+select *,crypt(concat(email,'!'), gen_salt('bf', 12)),crypt(concat(email,'!'),crypt(concat(email,'!'), gen_salt('bf', 12)))=password_hash from sellup.manager where role='OWNER' and email <> 'test';
+
+-- OWNER 인사람들 사업자번호+a!로 비번변경
+UPDATE sellup.manager
+SET
+    password_hash = crypt(email || 'a!', gen_salt('bf', 12)),
+    updated_at = now()
+WHERE role = 'OWNER'
+  AND email <> 'test';
+
+update sellup.manager m
+set
+    password_hash = crypt(m.email || 'a!', gen_salt('bf', 12)),
+    updated_at = now()
+where exists (
+    select 1
+    from sellup.manager_store ms
+    where ms.manager_id = m.manager_id
+      and ms.role = 'OWNER'
+)
+and m.email <> 'test';
+
+SELECT
+    manager_id,
+    email,
+    crypt(email || 'a!', password_hash) = password_hash AS password_ok
+FROM sellup.manager
+WHERE role = 'OWNER'
+  AND email <> 'test';
