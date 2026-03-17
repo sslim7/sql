3186af18ac5c143feed9927c2af806d104062a98
maxconomy <max@conomy.io>
2026-03-17 09:36:11 +0900

    매출업-빌링 추가

---
+++ monki/매출업-빌링.sql
@@ -0,0 +1,9 @@
+select * from sellup.basic_info;
+create table billings
+-- store_no
+-- start_billing_dt
+-- status (1.대기, 2.진행중, 3.중단, 4.취소) 디폴트 1
+-- process_type (1.자동 2.컨펌) 디폴트 1
+-- payment_method (1.CMS 2.credit_card) 디폴트 1
+-- pricing_type (1.basic, 2.
+-- price
